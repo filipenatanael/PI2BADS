edab57db8ecf745940ff57c18ed54b683dcb5478
Filipe Natanael <filipenatanael@live.com>
2017-10-23 23:49:21 -0200

    Creating LoginForm component
    Creating LoginForm component

--- TroK/src/Components/Login.js
+++ TroK/src/Components/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Image } from 'react-native';
+import { View, StyleSheet, Image, Text } from 'react-native';
 
 export default class Login extends Component {
   render() {
@@ -8,7 +8,9 @@ export default class Login extends Component {
         <View style={styles.logoContainer}>
           <Image
           style={styles.logo}
-          source={require('../images/logoTrok.jpeg')} />
+          source={require('../images/logoTrok.jpeg')}
+          />
+          <Text style={styles.title}>Hello, Welcome to the TroK.</Text>
         </View>
         <View style={styles.form} />
 
@@ -17,19 +19,25 @@ export default class Login extends Component {
   }
 }
 
-<View></View>
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
   },
   logoContainer: {
-
+    alignItems: 'center',
+    flexGrow: 1,
+    justifyContent: 'center',
   },
   logo: {
     width: 100,
     height: 100,
+  },
+  title: {
+    color: '#000',
+    marginTop: 10,
+    textAlign: 'center',
+    opacity: 0.5,
   },
   form: {
 
