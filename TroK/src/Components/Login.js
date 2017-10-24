1ddbb96ddf61a50803590fbbce06a018c8f19c01
Filipe Natanael <filipenatanael@live.com>
2017-10-23 23:31:30 -0200

    Upgrade login component
    Working in the field logo

--- TroK/src/Components/Login.js
+++ TroK/src/Components/Login.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 
 export default class Login extends Component {
   render() {
     return (
-      <View>
-      <Text> Hello </Text>
+      <View style={styles.container}>
+        <View style={styles.logoContainer}>
+          <Image
+          style={styles.logo}
+          source={require('../images/logoTrok.jpeg')} />
+        </View>
+        <View style={styles.form} />
+
       </View>
     );
   }
 }
+
+<View></View>
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  logoContainer: {
+
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  form: {
+
+  }
+});
