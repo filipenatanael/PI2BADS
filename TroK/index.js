383958d444235819ceb0df7905ebb73504d82d4b
Filipe Natanael <filipenatanael@live.com>
2017-10-25 23:38:19 -0200

    Initialize Firebase Testing
    Initialize Firebase Testing

--- TroK/index.js
+++ TroK/index.js
@@ -1,15 +1,31 @@
+import firebase from 'firebase';
 import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
-
-import Login from './src/Components/Login';
+import { AppRegistry, View, Text } from 'react-native';
+//import Login from './src/Components/Login';
 
 
 export default class App extends Component {
-     render() {
-       return (
-         <Login />
-       );
-     }
+
+  componentWillMount() {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyCd5GYkC7z5MSsymZQ0aaVpJXzySWp3Ocw",
+      authDomain: "trok-5e864.firebaseapp.com",
+      databaseURL: "https://trok-5e864.firebaseio.com",
+      projectId: "trok-5e864",
+      storageBucket: "trok-5e864.appspot.com",
+      messagingSenderId: "341467984322"
+    };
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+    <View>
+    <Text>My Application</Text>
+    </View>
+    );
+  }
 }
 
 
