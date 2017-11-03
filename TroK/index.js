1794a23832674f1e5f2e4d03de6125b764594ff0
Filipe Natanael <filipenatanael@live.com>
2017-11-02 23:37:07 -0200

    Installed Redux and Provider
    Installed Redux and Provider

--- TroK/index.js
+++ TroK/index.js
@@ -1,8 +1,21 @@
+import firebase from 'firebase';
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import Routes from './src/Routes';
 
 export default class App extends Component {
+  componentWillMount() {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyCd5GYkC7z5MSsymZQ0aaVpJXzySWp3Ocw",
+      authDomain: "trok-5e864.firebaseapp.com",
+      databaseURL: "https://trok-5e864.firebaseio.com",
+      projectId: "trok-5e864",
+      storageBucket: "trok-5e864.appspot.com",
+      messagingSenderId: "341467984322"
+    };
+    firebase.initializeApp(config);
+  }
 
   render() {
     return (
