d8ad5e2f17258436a4e16e658fc098687631b9a8
Filipe Natanael <filipenatanael@live.com>
2017-11-12 14:24:09 -0200

    Created file to separate style sheet
    Created file to separate style sheet

--- TroK/src/Components/Home.js
+++ TroK/src/Components/Home.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Image, Dimensions, TouchableOpacity } from 'react-native';
+import { View, Image, TouchableOpacity } from 'react-native';
 import CardScreen from './CardScreen';
 
-const { width, height } = Dimensions.get('window');
+import styles from './Styles';
 
 export default class Home extends Component {
   render() {
@@ -22,26 +22,3 @@ export default class Home extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#EAECEE'
-  },
-  navbar: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#29088A',
-  },
-  content: {
-    flex: 10
-  },
-  img: {
-    width: width / 10,
-    height: height / 14,
-  },
-  icon: {
-    justifyContent: 'center',
-    marginLeft: 10
-  }
-});
---
+++ TroK/src/Components/Styles.js
@@ -0,0 +1,28 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EAECEE'
+  },
+  navbar: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#29088A',
+  },
+  content: {
+    flex: 10
+  },
+  img: {
+    width: width / 10,
+    height: height / 14,
+  },
+  icon: {
+    justifyContent: 'center',
+    marginLeft: 10
+  }
+});
+
+export default styles;
