bf630d093b5562ff28a0eea09e2118b0719ddf8c
Filipe Natanael <filipenatanael@live.com>
2017-11-12 00:15:01 -0200

    Creating DrawerNavigator Update
    Creating DrawerNavigator Update

--- TroK/src/Components/AboutUs.js
+++ TroK/src/Components/AboutUs.js
@@ -1,33 +1,50 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image, Dimensions, TouchableOpacity } from 'react-native';
+import CardScreen from './CardScreen';
+
+const { width, height } = Dimensions.get('window');
 
 export default class AboutUs extends Component {
-        render() {
-          return (
-            <View style={styles.container}>
-            <View style={styles.navbar}>
-
-            </View>
-            <View style={styles.content}>
-             <Text>Body oof application</Text>
-            </View>
-            </View>
-          );
-        }
+  render() {
+    return (
+      <View style={styles.container}>
+      <View style={styles.navbar}>
+      <View style={styles.icon}>
+      <TouchableOpacity onPress={() => this.props.navigation.navigate('DrawerOpen')}>
+      <Image style={styles.img} source={require('../images/icones.png')} />
+      </TouchableOpacity>
+      </View>
+      </View>
+      <View style={styles.content}>
+      <CardScreen />
+      </View>
+      </View>
+    );
+  }
 }
 
 //  <Text style={{ textAlign: 'center', fontSize: 20, fontWeight: 'bold', color: '#fff', padding: 12 }} onPress={() => this.props.navigation.navigate('DrawerOpen')}></Text>
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#EAECEE'
-    },
-    navbar: {
-      flex: 1,
-      backgroundColor: '#29088A'
-    },
-    content: {
-      flex: 10
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#EAECEE'
+  },
+  navbar: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#29088A',
+  },
+  content: {
+    flex: 10
+  },
+  img: {
+    width: width / 10,
+    height: height / 14,
+  },
+  icon: {
+    justifyContent: 'center',
+    marginLeft: 10
+  }
+
 
 });
--- TroK/src/Components/Welcome.js
+++ TroK/src/Components/Welcome.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
-import { View, Text, Button, Icon, Image, TouchableHightlight, StyleSheet } from 'react-native';
+import { View, Text, Button, Icon, Image, TouchableHightlight, StyleSheet, ScrollView } from 'react-native';
 
-import { StackNavigator, DrawerNavigator } from 'react-navigation';
+import { StackNavigator, DrawerNavigator, DrawerItems } from 'react-navigation';
 
 
-import SecondScreen from '../../SecondScreen';
 import ItemList from './ItemList';
 import CardScreen from './CardScreen';
 import AboutUs from './AboutUs';
+
+
 
 const Drawer = DrawerNavigator(
   {
     First: {
       path: '/',
       screen: AboutUs,
+      navigationOptions: {
+        drawerLabel: <View><Text style={{ color: '#fff' }}>Menu do aplicativo TroK </Text></View>,
+        drawerIcon: <Image source={require('../images/Background.png')} size={20} style={{ height: 100 }} />
+      },
     },
     List: {
       path: '/sent',
@@ -27,44 +32,44 @@ const Drawer = DrawerNavigator(
   {
     initialRouterName: 'First',
     drawerPosition: 'left',
-    drawerWidth: 200,
+    drawerWidth: 300,
+    contentComponent: props => <ScrollView><DrawerItems {...props} /></ScrollView>,
+    drawerBackgroundColor: 'transparent',
     contentOptions: {
       activeTintColor: 'red',
     }
-
-  }
-);
+  });
 
 
 
-export default class Welcome extends Component {
-  render() {
-    return (
+  export default class Welcome extends Component {
+    render() {
+      return (
 
         <Drawer />
 
-    );
+      );
+    }
   }
-}
 
 
-/*
-import React from 'react';
-import { View, Text, Button, Image } from 'react-native';
-import { Actions } from 'react-native-router-flux';
+  /*
+  import React from 'react';
+  import { View, Text, Button, Image } from 'react-native';
+  import { Actions } from 'react-native-router-flux';
 
-export default props => (
-<Image style={{ flex: 1, width: null }} source={require('../images/Background.png')}>
-<View style={{ flex: 1, padding: 15 }}>
-<View style={{ flex: 2, justifyContent: 'center', alignItems: 'center' }}>
-<Image style={{ width: 150, height: 150 }} source={require('../images/logoTrok.jpeg')} />
-<Text style={{ color: '#fff', fontSize: 15, textAlign: 'center', marginTop: 10 }}>Welcome to TroK!</Text>
-</View>
-<View style={{ flex: 1 }}>
-<Button title="Get in" onPress={() => Actions.loginScreen()} />
-</View>
-</View>
-</Image>
+  export default props => (
+  <Image style={{ flex: 1, width: null }} source={require('../images/Background.png')}>
+  <View style={{ flex: 1, padding: 15 }}>
+  <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center' }}>
+  <Image style={{ width: 150, height: 150 }} source={require('../images/logoTrok.jpeg')} />
+  <Text style={{ color: '#fff', fontSize: 15, textAlign: 'center', marginTop: 10 }}>Welcome to TroK!</Text>
+  </View>
+  <View style={{ flex: 1 }}>
+  <Button title="Get in" onPress={() => Actions.loginScreen()} />
+  </View>
+  </View>
+  </Image>
 );
 
 
